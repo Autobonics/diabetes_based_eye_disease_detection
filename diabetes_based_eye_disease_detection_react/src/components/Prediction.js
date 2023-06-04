290cf5fe7d3c97d5f26e581bf8002e912e7650a6
v4zha <arvin_j4848@protonmail.com>
2023-06-04 23:28:14 +0530

    v2-fastapi-prediction

--- diabetes_based_eye_disease_detection_react/src/components/Prediction.js
+++ diabetes_based_eye_disease_detection_react/src/components/Prediction.js
@@ -1,54 +1,32 @@
 import React, { useState } from 'react';
-import * as tf from '@tensorflow/tfjs';
 import { Button, LinearProgress, Typography } from "@mui/material";
 
-const PredictionComponent = ({link, decease}) => {
+const PredictionComponent = ({ link, decease }) => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [predictionResult, setPredictionResult] = useState('');
-  const [predictionResultList, setPredictionResultList] = useState('');
-
-  const loadImage = (file) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        const img = new Image();
-        img.onload = () => {
-          resolve(img);
-        };
-        img.onerror = (error) => {
-          reject(error);
-        };
-        img.src = event.target.result;
-      };
-      reader.onerror = (error) => {
-        reject(error);
-      };
-      reader.readAsDataURL(file);
-    });
-  };
-
-
   const [loading, setLoading] = useState(false);
 
-
-  const loadModel = async () => {
-    const model = await tf.loadLayersModel(link);
-    performPrediction(model);
-  };
-
-  const performPrediction = async (model) => {
+  const getPrediction = async () => {
     setLoading(true);
-    const img = await loadImage(selectedImage);
-    setLoading(false);
-    const tensorImg = tf.browser.fromPixels(img);
-    const resizedImg = tf.image.resizeBilinear(tensorImg, [224, 224]);
-    const processedImg = resizedImg.expandDims(0).toFloat().div(255);
-    const prediction = model.predict(processedImg);
-    const result = prediction.dataSync(); // Get the prediction result as an array
-    console.log(result);
-    setPredictionResult(`Class1: ${result[0]}, Class2 ${result[1]}`); // Update the state with the prediction result
-    setPredictionResultList(result)
-    tf.dispose([img, resizedImg, processedImg, prediction]);
+
+    const formData = new FormData();
+    formData.append('file', selectedImage);
+
+    fetch('http://localhost:8000/predict', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        const predictedClass = data.predicted_class;
+        setPredictionResult(`Predicted Class: ${predictedClass}`);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setLoading(false);
+      });
   };
 
   const handleImageUpload = (event) => {
@@ -56,39 +34,33 @@ const PredictionComponent = ({link, decease}) => {
     setSelectedImage(file);
   };
 
-
-
   return (
     <div>
-        {loading && (
-          <LinearProgress
-            sx={{
-              position: "absolute",
-              top: { xs: "58px", sm: "64px", md: "64px" },
-              left: 0,
-              right: 0,
-              zIndex: 1,
-            }}
-          />
-        )}
+      {loading && (
+        <LinearProgress
+          sx={{
+            position: "absolute",
+            top: { xs: "58px", sm: "64px", md: "64px" },
+            left: 0,
+            right: 0,
+            zIndex: 1,
+          }}
+        />
+      )}
 
       <input type="file" accept="image/*" onChange={handleImageUpload} />
-      <Button variant="contained" color="primary" onClick={loadModel}>
+      <Button variant="contained" color="primary" onClick={getPrediction}>
         Predict
       </Button>
       {predictionResult && (
-        <Typography variant="h6"
-        sx={{marginTop: "20px"}}
-        >
+        <Typography variant="h6" sx={{ marginTop: "20px" }}>
           {predictionResult}
         </Typography>
       )}
 
-        {predictionResult && (
-        <Typography variant="h4"
-        sx={{marginTop: "20px"}}
-        >
-          Prediction Result: {predictionResultList[0] < predictionResultList[1] ? `${decease}` : `No ${decease}`}
+      {predictionResult && (
+        <Typography variant="h4">
+          Prediction Result: {predictionResult}
         </Typography>
       )}
     </div>
